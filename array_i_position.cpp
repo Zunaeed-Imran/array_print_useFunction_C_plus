20ed56a3c53f3772b7e5f6be8c1c63ed57e7e998
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-08 17:09:04 +0600

    array_print_arrayValue_useFunction_C_++

---
+++ array_i_position.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+int arr() {
+  int array[7] = {7,95,5,41,78,54,2};
+    for (int i = 1; i <= 7; i++) {
+      cout << array[i] << " ";
+    }
+}
+
+
+int main () {
+  arr();
+
+  return 0;
+}
